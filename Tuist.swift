6ccf20d98988194c2405be940a06af341249ead5
modeezie <81447746+modeezie@users.noreply.github.com>
2026-06-23 23:47:12 -0500

    Update compatible Xcode version to 27.0.0
    Signed-off-by: modeezie <81447746+modeezie@users.noreply.github.com>

--- Tuist.swift
+++ Tuist.swift
@@ -4,7 +4,7 @@ let config: Config = .init(
     project: .tuist(
         compatibleXcodeVersions: .list([
             .upToNextMajor(.init(16, 0, 0)),
-            .upToNextMajor(.init(26, 0, 0))
+            .upToNextMajor(.init(27, 0, 0))
         ]),
         swiftVersion: .init(6, 0, 0),
         plugins: .init(),
